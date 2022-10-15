6a1d60b6532f9dc6d3cbf7e4e87ae7152973a177
Gabriel Fioravante <61801407+gabrielfioravante@users.noreply.github.com>
2022-10-15 05:36:51 -0300

    feat(neovim): update cmp mappings

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -16,10 +16,11 @@ cmp.setup({
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
     }),
     sources = {
         { name = "nvim_lsp" },
