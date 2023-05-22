cdb9aead884f42b9f34d3120d8a318f155ea17e6
Gabriel Fioravante <61801407+gabrielfioravante@users.noreply.github.com>
2023-05-22 07:19:22 -0300

    feat(neovim): add harpoon and new mappings

--- nvim/.config/nvim/plugin/mappings.lua
+++ nvim/.config/nvim/plugin/mappings.lua
@@ -1,5 +1,11 @@
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
 
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
