a0013e67656b9dee730a519751666afeadae386a
Gabriel Fioravante <61801407+gabrielfioravante@users.noreply.github.com>
2023-05-23 21:32:28 -0300

    feat(neovim): update telescope's mappings

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,32 +1,38 @@
 local telescope = require("telescope")
 
 telescope.setup {
-  defaults = {
-    file_ignore_patterns = {"node_modules"},
-  },
-  preview = {
-    filesize_limite = 5,
-    treesitter = true,
-  },
-  pickers = {
-    buffers = {
-      show_all_buffers = true,
-      sort_lastused = true,
-      previewer = false,
-      theme = "dropdown",
-      mappings = {
-        i = { ["<c-d>"] = require("telescope.actions").delete_buffer, },
-        n = { ["<c-d>"] = require("telescope.actions").delete_buffer, }
-      }
-    }
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-    }
-  },
+    defaults = {
+        file_ignore_patterns = { "node_modules" },
+    },
+    preview = {
+        filesize_limite = 5,
+        treesitter = true,
+    },
+    pickers = {
+        buffers = {
+            show_all_buffers = true,
+            sort_lastused = true,
+            previewer = false,
+            theme = "dropdown",
+            mappings = {
+                i = { ["<c-d>"] = require("telescope.actions").delete_buffer, },
+                n = { ["<c-d>"] = require("telescope.actions").delete_buffer, }
+            }
+        }
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+        }
+    },
 }
 
 telescope.load_extension("fzf")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
--- nvim/.config/nvim/plugin/mappings.lua
+++ nvim/.config/nvim/plugin/mappings.lua
@@ -10,8 +10,3 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "<leader>gs", "<cmd>lua require('neogit').open()<CR>")
-
-vim.keymap.set("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
-vim.keymap.set("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
-vim.keymap.set("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>")
-vim.keymap.set("n", "<C-p>", "<cmd>lua require('telescope.builtin').git_files()<CR>")
