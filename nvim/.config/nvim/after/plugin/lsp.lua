1de0ebdcbc889a19612887690a5277bdeca95e32
Gabriel Mônego <gabriel.monego@f1solucoes.com.br>
2023-05-27 18:19:48 -0300

    feat(nvim): use clippy

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -20,9 +20,8 @@ cmp.setup({
     }
 })
 
-lsp.on_attach(function(_, bufnr)
+local on_attach = function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
-
     vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
@@ -36,7 +35,21 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>fm', vim.lsp.buf.format, opts)
-end)
+end
+
+lsp.on_attach(on_attach)
+
+require'lspconfig'.rust_analyzer.setup {
+    on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            cmd = { "rustup", "run", "nightly", "rust-analyzer" },
+            checkOnSave = {
+                command = "clippy"
+            },
+        }
+    }
+}
 
 
 lsp.setup()
