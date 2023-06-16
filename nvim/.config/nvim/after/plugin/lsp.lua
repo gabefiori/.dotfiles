4a8db3eda461a07a271ea56a250be789136d2c6c
Gabriel Mônego <gabriel.monego@f1solucoes.com.br>
2023-06-16 15:59:26 -0300

    feat(nvim): use trouble instead of lsp for references

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -7,19 +7,19 @@ lsp.setup_servers = ({ 'gopls', 'pyright', 'tsserver', 'intelephense', 'html', '
 local cmp = require('cmp')
 local _ = require('lsp-zero').cmp_action()
 
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 local on_attach = function(_, bufnr)
@@ -30,15 +30,19 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>fm', vim.lsp.buf.format, opts)
+
+    -- Silent
+    opts.silent = true
+    vim.keymap.set("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", opts)
 end
 
 lsp.on_attach(on_attach)
 
-require'lspconfig'.rust_analyzer.setup {
+require 'lspconfig'.rust_analyzer.setup {
     on_attach = on_attach,
     settings = {
         ["rust-analyzer"] = {
@@ -51,3 +55,7 @@ require'lspconfig'.rust_analyzer.setup {
 
 
 lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
