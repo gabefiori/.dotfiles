c53ce83ce253d663f3cab076b18a7f885003758f
Gabriel Fioravante <61801407+gabrielfioravante@users.noreply.github.com>
2024-02-10 14:28:21 -0300

    fix(tmux-sessionizer): check if tmux is attached

--- scripts/scripts/tmux-sessionizer.sh
+++ scripts/scripts/tmux-sessionizer.sh
@@ -22,8 +22,8 @@ if ! tmux has-session -t=$selected_name 2> /dev/null; then
     tmux new-session -ds $selected_name -c $selected
 fi
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux a
+if [[ -n $TMUX ]]; then
+    tmux switch-client -t $selected_name
+else
+    tmux attach-session -t $selected_name
 fi
-
-tmux switch-client -t $selected_name 2> /dev/null
