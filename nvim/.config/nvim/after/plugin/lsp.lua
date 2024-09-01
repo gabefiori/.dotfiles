45af5709e6903cf4c6eeee85e60b277234b0db1b
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2024-09-01 07:15:17 -0300

    feat(nvim): update completion

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -20,6 +20,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
+vim.diagnostic.config({
+    virtual_text = true,
+})
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
@@ -71,28 +75,4 @@ require("mason-lspconfig").setup({
     },
 })
 
-local cmp = require("cmp")
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    sources = {
-        { name = "path" },
-        { name = "nvim_lsp" },
-        { name = " luasnip" }
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-    }),
-})
-
-vim.diagnostic.config({
-    virtual_text = true,
-})
+require("lspconfig").gleam.setup({})
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -9,6 +9,21 @@ require("lazy").setup({
     { "L3MON4D3/LuaSnip" },
     { "saadparwaiz1/cmp_luasnip" },
 
+    -- completion
+    {
+        "hrsh7th/nvim-cmp",
+        lazy = false,
+        priority = 100,
+        dependencies = {
+            "onsails/lspkind.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+            { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+            "saadparwaiz1/cmp_luasnip",
+        },
+    },
+
     -- Trouble
     {
         "folke/trouble.nvim",
