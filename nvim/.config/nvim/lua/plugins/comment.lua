44eaabac6378ac5b1664b885b3d33efd674da0b1
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2024-09-13 05:56:36 -0300

    chore(nvim): remove unused

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -2,25 +2,5 @@ return {
     {
         "numToStr/Comment.nvim",
         opts = {},
-    },
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {
-            --TODO:
-            --PERF:
-            --FIXME:
-            --BUG:
-            --NOTE:
-            --HACK:
-            --WARNING:
-            signs = false,        -- show icons in the signs column
-            highlight = {
-                multiline = true, -- enable multine todo comments
-                before = "",      -- "fg" or "bg" or empty
-                keyword = "bg",   -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-                after = "",       -- "fg" or "bg" or empty
-            },
-        }
     }
 }
