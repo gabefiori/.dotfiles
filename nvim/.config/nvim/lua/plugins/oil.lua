49eb02e4b2fbf299174027fc8c2938df7a903583
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2024-09-13 06:14:34 -0300

    fix(neovim): disable oil's default keymaps

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -7,6 +7,7 @@ return {
                 view_options = {
                     show_hidden = true,
                 },
+                use_default_keymaps = true,
             }
 
             vim.keymap.set("n", "<C-n>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
