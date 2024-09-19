dd0f9fcf64087b356faa5f6b23359a23baa2a51d
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2024-09-19 16:42:28 -0300

    feat(nvim): update completion

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -7,34 +7,40 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-buffer",
+            "onsails/lspkind.nvim",
         },
-        config = function()
-            local cmp = require("cmp")
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-            cmp.setup({
+        config = function()
+            vim.opt.completeopt = { "menu", "menuone", "noselect" }
+            vim.opt.shortmess:append "c"
+
+            local cmp = require "cmp"
+
+            cmp.setup {
+                sources = {
+                    { name = "nvim_lsp", keyword_length = 4 },
+                    { name = "buffer",   keyword_length = 4 },
+                    { name = "path" },
+                },
+
+                mapping = {
+                    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+                    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+                    ["<C-y>"] = cmp.mapping(
+                        cmp.mapping.confirm {
+                            behavior = cmp.ConfirmBehavior.Insert,
+                            select = true,
+                        },
+                        { "i", "c" }
+                    ),
+                },
+
                 snippet = {
                     expand = function(args)
                         vim.snippet.expand(args.body)
                     end,
                 },
-
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                }),
-
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'path' },
-                    { name = 'buffer' },
-                }),
-            })
-
-            vim.opt.completeopt = { "menu", "menuone", "noselect" }
-            vim.opt.shortmess:append "c"
+            }
         end,
     },
 }
