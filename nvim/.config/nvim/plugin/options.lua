6f981b0b350eb8ee9f4b6ec9a2ca9d5147db61ea
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2024-09-30 07:46:54 -0300

    feat: kanagawa

--- nvim/.config/nvim/plugin/options.lua
+++ nvim/.config/nvim/plugin/options.lua
@@ -1,21 +1,10 @@
 local opt = vim.opt
 
--- Highlight yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('HighlightYank', {}),
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
-})
-
 opt.wrap = false
 opt.number = true
 opt.relativenumber = true
 opt.signcolumn = "yes"
+opt.colorcolumn = "80"
 opt.guicursor = ''
 
 opt.smartcase = true
