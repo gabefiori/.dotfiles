eea042b9c88f5946cb68ec2a0b0a24e41617ce7b
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2025-05-03 17:31:29 -0300

    feat: using sway

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,7 @@ set -U fish_user_paths $HOME/.cargo/bin/ $fish_user_paths
 set -U fish_user_paths /usr/local/go/bin $fish_user_paths
 set -U fish_user_paths $HOME/go/bin $fish_user_paths
 set -U fish_user_paths $HOME/.odin/ $fish_user_paths
-set -U fish_user_paths $HOME/.local $fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 set -U fish_user_paths $HOME/scripts $fish_user_paths
 
 set -gx EDITOR nvim
