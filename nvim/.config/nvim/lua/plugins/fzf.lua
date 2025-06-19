aae55f011401f52ca4e708bec3257e6975cf8510
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2025-06-19 03:12:02 -0300

    Add icons to fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,5 +1,6 @@
 return {
     "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         local fzf = require("fzf-lua")
         fzf.setup({ "border-fused" })
