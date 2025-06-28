505fc9856881d52d29f5fb9e80e0f7dc4d4a57d4
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2025-06-28 00:26:52 -0300

    Update configs

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,9 +1,8 @@
 return {
     "ibhagwan/fzf-lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         local fzf = require("fzf-lua")
-        fzf.setup({ "borderless" })
+        fzf.setup({ "max-perf" })
 
         vim.keymap.set("n", "<leader>ff", fzf.files, {})
         vim.keymap.set("n", "<leader>fw", fzf.grep_cword, {})
